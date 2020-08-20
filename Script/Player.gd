c02d35ca2fae167179d35ba8a575392c1d9dc3f6
DESKTOP-H0KFHFV\wuwei <1020655014@qq.com>
2020-08-20 18:10:31 +0800

    这是个坑

---
+++ Script/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var vec=Vector2()
+
+export var Speed=100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var x=Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	var y=Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
+	vec=Vector2(x,y)
+	if vec.x>0:
+		$AnimationPlayer.play("rigth")
+	if vec.x<0:
+		$AnimationPlayer.play("left")
+	if vec.y>0:
+		$AnimationPlayer.play("down")
+	if vec.y<0:
+		$AnimationPlayer.play("up")
+	move_and_slide(vec*300)
+	print(vec)
+	pass
